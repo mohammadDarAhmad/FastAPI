941e5184124b4dba91dfbb3fa976a517b921aaa1
mohammadDarAhmad <mohammadhamid757@gmail.com>
2021-03-19 19:56:10 +0200

    Add APIs and status code to the API

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
-from . import schemas, models,database
+from . import schemas, models, database
 from .database import engine
 from sqlalchemy.orm import Session
-from fastapi import Depends
+from fastapi import FastAPI, Depends, status , HTTPException
 
 
 app = FastAPI()
@@ -14,10 +13,24 @@ models.Base.metadata.create_all(engine)
 get_db = database.get_db
 
 
-@app.post('/blog')
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db), ):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog', status_code=status.HTTP_200_OK)
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}',  status_code=status.HTTP_204_NO_CONTENT )
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This is not available")
+    return blog
