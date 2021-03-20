7c101ade9246ed5afa544b48402265e90c110cdf
mohammadDarAhmad <mohammadhamid757@gmail.com>
2021-03-20 11:46:10 +0200

    Add APIs to blog

--- blog/main.py
+++ blog/main.py
@@ -34,3 +34,28 @@ def show(id, db: Session = Depends(get_db)):
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This is not available")
     return blog
+
+
+@app.delete('/blog/{id}')
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}')
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.update(request)
+    db.commit()
+    return 'updated'
+
+
+
